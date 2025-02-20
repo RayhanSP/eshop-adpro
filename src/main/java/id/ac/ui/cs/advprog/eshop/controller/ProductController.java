bdafffdcb29a423f294e41a0d89488737703cc46
RayhanSP <ray.han2005.rh@gmail.com>
2025-02-20 22:37:12 +0700

    remove koyeb-deploy.yml and fix typo in ProductController

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -21,7 +21,7 @@ public class ProductController {
     public String createProductPage(Model model) {
         Product product = new Product();
         model.addAttribute("product", product);
-        return "createProduct";
+        return "CreateProduct";
     }
 
     @PostMapping("/create")
@@ -37,7 +37,7 @@ public class ProductController {
     public String productListPage(Model model) {
         List<Product> allProducts = service.findAll();
         model.addAttribute("products", allProducts);
-        return "productList";
+        return "ProductList";
     }
 
     @GetMapping("/edit/{id}")
@@ -49,7 +49,7 @@ public class ProductController {
 
             if (product.getProductId() != null && product.getProductId().equals(productId)) {
                 model.addAttribute("product", product);
-                return "editProduct";
+                return "EditProduct";
             }
         }
 
